9bfcc362efdc797c235fc4844f4fcd58091ab7f2
kinarob <kinarobin@outlook.com>
2018-10-30 17:53:10 +0800

    delete zhushi

--- src/test/java/TablePropGetTest.java
+++ src/test/java/TablePropGetTest.java
@@ -23,10 +23,10 @@ public class TablePropGetTest {
             e.printStackTrace();
         }
 
-        //2.  利用SqlSessionFactory打开一个和数据库的SqlSession
+        
         SqlSession session = sqlSessionFactory.openSession();
 
-        //3. 利用这个SqlSession获取要使用的mapper接口
+        
         TableQueryMapper accountMapper = session.getMapper(TableQueryMapper.class);
 
         //4. 使用mapper接口和数据库交互，运行mapper.xml文件中的SQL语句
